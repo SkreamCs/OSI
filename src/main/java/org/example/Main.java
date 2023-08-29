165293a5e4a56dc163ef2d9816ee1140e2796233
Emma Parisgit config --global user.name Skream <tumho00005@gmail.com>
2023-08-29 15:59:22 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int port = 8080;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();) {
+                    // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+                     // ждем подключения
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    System.out.println("New connection accepted");
+
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+                } catch (IOException e) {
+                    e.getMessage();
+                }
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+}
